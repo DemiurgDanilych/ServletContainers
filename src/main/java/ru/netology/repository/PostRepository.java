a0502f04456d8d762ba871d1ab20dda7cb7b985c
Danil Agafonov <javaDanilAgafonov@yandex.ru>
2022-05-29 10:08:22 +0300

    Refactoring code

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -1,25 +1,52 @@
 package ru.netology.repository;
 
+import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
 
-// Stub
 public class PostRepository {
-  public List<Post> all() {
-    return Collections.emptyList();
-  }
+    private final AtomicLong count;
+    private final Map<Long, Post> repository;
 
-  public Optional<Post> getById(long id) {
-    return Optional.empty();
-  }
+    public PostRepository() {
+        long initiatCount = 1;
+        this.count = new AtomicLong(initiatCount);
+        this.repository = new ConcurrentHashMap<>();
+    }
 
-  public Post save(Post post) {
-    return post;
-  }
+    public List<Post> all() {
+        System.out.println(repository);
+        return repository.values().stream().filter(Objects::nonNull).collect(Collectors.toList());
+    }
 
-  public void removeById(long id) {
-  }
+    public Optional<Post> getById(long id) {
+        var post = repository.get(id);
+        if (post != null) {
+            return Optional.of(post);
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public Post save(Post post) {
+        long postId = post.getId();
+        if (postId == 0) {
+            post.setId(count.getAndIncrement());
+            repository.put(post.getId(),post);
+            return post;
+        } else if (repository.containsKey(postId)){
+            repository.put(postId,post);
+            return post;
+        }else
+        throw new NotFoundException();
+    }
+
+    public void removeById(long id) {
+        repository.remove(id);
+        throw new NotFoundException();
+    }
 }
